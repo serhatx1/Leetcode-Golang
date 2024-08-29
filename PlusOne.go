6fe93ba2a45bf87414182d6333fdfd37ed5e0b5d
Serhat Arslan <49308319+serhatx1@users.noreply.github.com>
2024-08-29 18:30:45 +0300

    Create PlusOne.go

---
+++ PlusOne.go
@@ -0,0 +1,21 @@
+func plusOne(digits []int) []int {
+    k:=1;
+    for i:=len(digits)-1;i>=0;i--{
+        if i==len(digits)-1 && digits[i]<9{
+            digits[i]=digits[i]+k
+            return digits
+            }  else if digits[i]+k>=10 && i!=0{
+                digits[i]=0
+                k=1
+            } else if digits[i]+k==10&&i==0{
+            digits[i]=1
+            digits=append(digits,0)
+            fmt.Println(digits)
+        }else{
+            digits[i]+=k
+            k=0
+        }
+      
+    }
+    return digits
+}
