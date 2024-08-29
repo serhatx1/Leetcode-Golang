d54c04992e9b62af8d9b7b66f0ba86be109cd7e6
Serhat Arslan <49308319+serhatx1@users.noreply.github.com>
2024-08-29 18:34:57 +0300

    Update PlusOne.go

--- PlusOne.go
+++ PlusOne.go
@@ -1,6 +1,29 @@
 func plusOne(digits []int) []int {
     k:=1;
     for i:=len(digits)-1;i>=0;i--{
+       if digits[i]+k<10{
+        digits[i]+=k
+        k=0
+       }else if i!=0{
+        digits[i]=0
+        k=1
+       }else if i==0{
+        digits[i]=k
+        digits=append(digits,0)
+       }
+       
+    }
+    return digits
+}
+
+
+
+
+
+
+func plusOne(digits []int) []int {
+    k:=1;
+    for i:=len(digits)-1;i>=0;i--{
         if i==len(digits)-1 && digits[i]<9{
             digits[i]=digits[i]+k
             return digits
